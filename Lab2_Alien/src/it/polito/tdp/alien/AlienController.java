69bf041cb133af7a2596d65042bf89ef0b687bb3
Ste <Ste@172.20.232.115>
2017-03-15 13:47:44 +0100

    completato punto1

--- Lab2_Alien/src/it/polito/tdp/alien/AlienController.java
+++ Lab2_Alien/src/it/polito/tdp/alien/AlienController.java
@@ -9,7 +9,11 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class AlienController {
+	
+	private String stringaInserita;
 
+	private AlienDictionary ad = new AlienDictionary();
+	
     @FXML
     private ResourceBundle resources;
 
@@ -30,14 +34,46 @@ public class AlienController {
 
     @FXML
     void doReset(ActionEvent event) {
-
+    	txtInsert.clear();
+    	txtResult.clear();
+    	ad.getDizionarioM().clear();
     }
 
     @FXML
     void doTranslate(ActionEvent event) {
-
+    	stringaInserita="";
+    	stringaInserita = txtInsert.getText();
+    	stringaInserita = stringaInserita.toLowerCase();
+    	String [] st = stringaInserita.split(" ");
+    	
+    	if (stringaInserita.matches(".*[a-zA-Z].*")==false){
+    	
+    	
+    	if (st.length==2){
+    	String alienWord = st[0];
+    	String translation = st[1];
+    	
+    	ad.addWord(alienWord, translation);
+    	
+    	txtResult.setText("Parola aggiunta con successo!\n"+ad.getDizionarioM().get(alienWord).getAlienWord()+" "+ad.getDizionarioM().get(alienWord).getTranslation());
+    	}
+    	
+    	if (st.length==1){
+    		String alienWord = st[0];
+    		if (ad.getDizionarioM().containsKey(alienWord)==false){
+    			txtResult.setText("Parola non presente nel dizionario");
+    		}
+    		else{
+    		txtResult.setText(ad.translateWord(alienWord));
+    	}
+    	}
+    
     }
-
+    	
+    else{
+    	txtResult.setText("Inserisci una parola valida");
+    }
+    }
     @FXML
     void initialize() {
         assert txtInsert != null : "fx:id=\"txtInsert\" was not injected: check your FXML file 'Alien.fxml'.";
